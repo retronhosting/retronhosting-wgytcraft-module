0b55aebe611775bc01afd0886a126871aadf7d08
William (Wgyt) Horning <williamhorning@wgyt.tk>
2021-03-11 20:27:16 -0500

    retronhosting module

---
+++ index.js
@@ -0,0 +1,19 @@
+fs = require('fs-extra')
+module.exports = function (host, res, req, error, version, ejs, config) {
+    /*!
+        retronhosting in wgytcraft
+        licensed under the mit license, 2021 William Horning (@wgyt)
+        retronhosting is owned by @retronbv
+    */
+   var dirname = config.pagedir
+   var page404 = config["404"]
+   doesitexist = fs.pathExists(`${dirname + res.url}.html`, (err, exists) => {console.log(err);return exists;})
+   if (doesitexist){
+       res.writeHead(200)
+       res.write(fs.readFileSync(`${dirname + res.url}.html`, "utf8"))
+   }else{
+    res.writeHead(404)
+    res.write(fs.readFileSync(`${dirname + page404}.html`), "utf8")
+   }
+   res.end()
+}
